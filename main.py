bdc54750baa9dec9d474a59402d5b9d2f674bddc
XIKAKY <danil.fedorov.75@inbox.ru>
2026-03-18 20:05:48 +0300

    Add new functions to main.py
    y

--- main.py
+++ main.py
@@ -1,2 +1,33 @@
-import rt
-rt.rating()
+password = input("Enter your password: ")
+
+def is_very_long(lenght):
+    return len(lenght) >= 12
+
+def has_digit(word):
+   return any(c.isdigit() for c in word)
+
+def has_words(word):
+    return any(c.isalpha() for c in word)
+
+def has_lower(word):
+    return any(c.islower() for c in  word)
+
+def has_upper(word):
+   return any(c.isupper() for c in word)
+
+def has_symbols(digit):
+    return "%" in digit or "#" in digit
+
+functions = [is_very_long, has_digit, has_words, has_upper, has_lower, has_symbols]
+
+
+def rating():
+    score = 0
+    for func in functions:
+
+        if func(password):
+            score += 2
+    print(score)
+
+if __name__ == "__main__":
+    rating()
